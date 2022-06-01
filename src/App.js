483aca87952a1911f739f91ba200f2ee94d52fd0
huakang <741180788@qq.com>
2022-06-01 16:42:04 +0800

    feat: container-pattern

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import DogImagesContainer from './components/DogImagesContainer';
 
 function App() {
   return (
     <div className="App">
+      <h1>
+        Browse Dog Images{" "}
+        <span role="img" aria-label="emoji">
+          🐕
+        </span>
+      </h1>
+      <DogImagesContainer />
     </div>
   );
 }
---
+++ src/components/DogImages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import useDogImages from "./useDogImages";
+
+// export default function DogImages({ dogs }) {
+//   return dogs.map((dog, i) => <img src={dog} key={i} alt="Dog" />);
+// }
+export default function DogImages() {
+  const dogs = useDogImages();
+
+  return dogs.map((dog, i) => <img src={dog} key={i} alt="Dog" />);
+}
